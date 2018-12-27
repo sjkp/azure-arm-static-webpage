2cef3c3e0ee61037978af06a891e0400fe9d6419
Simon Pedersen <blasimtex@hotmail.com>
2018-12-27 21:21:54 +0100

    escape $web

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 az extension add --name storage-preview
 az storage blob service-properties update --static-website --404-document error.html --index-document index.html
 
-az storage blob upload-batch -s /mnt/content/dist -d "$web"
+az storage blob upload-batch -s /mnt/content/dist -d "\$web"
